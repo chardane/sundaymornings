0b326b6e8c51518599ed5051032f6654573016ae
bluejay2011 <stephjose2011@gmail.com>
2016-10-27 16:03:10 +0800

    added factorial problem

---
+++ factorial/factorial.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+
+function factorial(x) {   
+  if (x === 0) {  
+    return 1;  
+  }  
+  
+  return x * factorial(x-1);             
+}  
+
+console.log(factorial(5));  
+</script>
