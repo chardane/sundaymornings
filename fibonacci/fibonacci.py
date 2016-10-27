01056ea220e305825cd70ace2a4814fcc75abb9f
Kan Ouivirach <kan@prontomarketing.com>
2016-10-27 09:22:10 +0700

    Write first fibo case

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(number):
+    """Return n'th element of Fibonacci sequence
+    >>> fibonacci(1)
+    [1]
+    """
+
+    return [1]
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
