44d9ebecac3f31d967ba8235b7ef20ffd9fcfba1
Michael Nielson <michael.nielson@freeflowdigital.com>
2016-10-26 18:18:16 -0700

    Added docs and Hard mode version.

--- count-to-100-and-back/count-to-100-and-back.js
+++ count-to-100-and-back/count-to-100-and-back.js
@@ -3,10 +3,17 @@
 
 */
 
+
+/*
+ * Increment the counter by one & return it.
+ */
 var incrementCounter = function(counter) {
   return counter += 1;
 }
 
+/*
+ * Decrement the counter by one & return it.
+ */
 var decrementCounter = function(counter) {
   return counter -= 1;
 }
@@ -25,4 +32,22 @@ var testCountToOneHundredAndBack = function() {
   }
 }
 
+var testCountToNAndBack = function(n) {
+  var counter = 1;
+  console.log("Going to "+n+" and back!");
+  console.log(counter);
+  while (counter < n) {
+    counter = incrementCounter(counter);
+    console.log(counter);
+  }
+  while (counter > 1) {
+    counter = decrementCounter(counter);
+    console.log(counter);
+  }
+
+}
+
 testCountToOneHundredAndBack();
+testCountToNAndBack(10);
+testCountToNAndBack(5);
+testCountToNAndBack(3);
