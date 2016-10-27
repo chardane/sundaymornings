68d0f5b917dada7c3d495c39d5826c01ab7ca4be
Kan Ouivirach <kan@prontomarketing.com>
2016-10-27 08:40:28 +0700

    Get only char in string that is digit

--- number-from-string/number_from_string.py
+++ number-from-string/number_from_string.py
@@ -2,8 +2,16 @@ def get_number_from_string(string):
     """ Return digit characters and parse the remaining to number
     >>> get_number_from_string('1')
     1
+    >>> get_number_from_string('This is number: 9')
+    9
     """
-    return int(string)
+
+    result = ''
+    for each in string:
+        if each.isdigit():
+            result += each
+
+    return int(result)
 
 
 if __name__ == "__main__":
