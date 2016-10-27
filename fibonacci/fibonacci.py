082acf3cc6bce43487eb3e43f1b76007857ccb78
Kan Ouivirach <kan@prontomarketing.com>
2016-10-27 09:23:09 +0700

    Make second case pass

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -2,9 +2,14 @@ def fibonacci(number):
     """Return n'th element of Fibonacci sequence
     >>> fibonacci(1)
     [1]
+    >>> fibonacci(2)
+    [1, 1]
     """
 
-    return [1]
+    if number == 1:
+        return [1]
+    if number == 2:
+        return [1, 1]
 
 
 if __name__ == "__main__":
